5ec0bcd2293d89b2ea81467d57451478071569d6
mujpao <mujpao@gmail.com>
2022-09-30 17:06:22 -0700

    Use a class instead

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 "use strict";
 
-const inputWithErrorMsgProto = {
+class InputWithErrorMsg {
+    constructor(id, errorMsg) {
+        this.id = id;
+        this.errorMsg = errorMsg;
+        this.inputElement = document.getElementById(id);
+        this.errorElement = document.querySelector(`#${id} + span.error`);
+        this.labelElement = document.querySelector(`label[for=${id}]`);
+
+        if (this.inputElement.value === "") {
+            this.labelElement.classList.remove("visually-hidden");
+        } else {
+            this.labelElement.classList.add("visually-hidden");
+        }
+
+        const errorID = `${id}-error`;
+
+        this.errorElement.id = errorID;
+    }
+
     isValid() {
         return this.inputElement.validity.valid;
-    },
+    }
 
     markTouched() {
         this.inputElement.classList.add("touched");
-    },
+    }
 
     showError() {
         this.errorElement.textContent = this.errorMsg;
         this.inputElement.setAttribute("aria-invalid", "true");
-        this.inputElement.setAttribute("aria-errormessage", this.errorID);
-    },
+        this.inputElement.setAttribute("aria-errormessage", this.errorElement.id);
+    }
 
     hideError() {
         this.errorElement.textContent = "";
         this.inputElement.setAttribute("aria-invalid", "false");
         this.inputElement.removeAttribute("aria-errormessage");
-    },
+    }
 
     addListeners() {
         this.inputElement.addEventListener("input", () => {
@@ -37,33 +55,14 @@ const inputWithErrorMsgProto = {
                 this.labelElement.classList.add("visually-hidden");
             }
         });
-    },
-};
-
-function inputWithErrorMsg(id, errorMsg) {
-    const inputElement = document.getElementById(id);
-    const errorElement = document.querySelector(`#${id} + span.error`);
-    const labelElement = document.querySelector(`label[for=${id}]`);
-
-    if (inputElement.value === "") {
-        labelElement.classList.remove("visually-hidden");
-    } else {
-        labelElement.classList.add("visually-hidden");
     }
-
-    const errorID = `${id}-error`;
-
-    errorElement.id = errorID;
-
-    return Object.assign(Object.create(inputWithErrorMsgProto),
-        { id, errorMsg, inputElement, errorElement, labelElement, errorID });
 }
 
 const inputs = [
-    inputWithErrorMsg("firstname", "First Name cannot be empty"),
-    inputWithErrorMsg("lastname", "Last Name cannot be empty"),
-    inputWithErrorMsg("emailaddr", "Looks like this is not an email"),
-    inputWithErrorMsg("password", "Password cannot be empty"),
+   new InputWithErrorMsg("firstname", "First Name cannot be empty"),
+   new InputWithErrorMsg("lastname", "Last Name cannot be empty"),
+   new InputWithErrorMsg("emailaddr", "Looks like this is not an email"),
+   new InputWithErrorMsg("password", "Password cannot be empty"),
 ];
 
 for (const input of inputs) {
